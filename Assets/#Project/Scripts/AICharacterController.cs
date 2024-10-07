c60ef3424a8b7c1324f6392c2a0b8ce4ac059fb1
Marie-Astrid Buelens <mabuelens@gmail.com>
2024-10-07 09:56:06 +0200

    Exercice state machine IA pursue-patrol

--- Assets/#Project/Scripts/AICharacterController.cs
+++ Assets/#Project/Scripts/AICharacterController.cs
@@ -10,11 +10,20 @@ using UnityEngine.AI;
 public class AICharacterController : MonoBehaviour
 {
     
+
     public Transform playerTransform;
     private NavMeshAgent navMeshAgent;
     public Transform waypointsGroup;//on passe le transform parent de tous les waypoints
 
     private Transform currentWaypoint;
+
+    public enum AIControllerState{
+        Patrol,
+        Pursue
+    }
+
+    public AIControllerState state;
+    private bool visiblePlayer = false;
 
     // Start is called before the first frame update
     void Start()
@@ -27,8 +36,24 @@ public class AICharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //navMeshAgent.SetDestination(playerTransform.position);
+        visiblePlayer = CheckPlayerVisibility();
+        if (visiblePlayer){
+            state = AIControllerState.Pursue;
+        } else{
+            state = AIControllerState.Patrol;
+        }
+
+        switch(state){
+            case AIControllerState.Patrol:
+                navMeshAgent.SetDestination(currentWaypoint.position);
+                break;
+            case AIControllerState.Pursue:
+                navMeshAgent.SetDestination(playerTransform.position);
+                break;
+        }
     }
+
+  
 
     Transform SelectDestination(){
         int index = Random.Range(0, waypointsGroup.childCount);
@@ -51,9 +76,11 @@ public class AICharacterController : MonoBehaviour
 
     bool CheckPlayerVisibility(){
         RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.forward * 10f);
         if(Physics.Raycast(transform.position, playerTransform.position - transform.position, out hit, 10f)){
             if(hit.collider.CompareTag("Player")){
                 if(Vector3.Angle(transform.forward, playerTransform.position - transform.position) < 45){
+                    Debug.Log("Vu !");
                     return true;
                 }
             }
